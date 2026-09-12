7fc8e1aab385305e530965971919ceed674373dd
Tilak <tilakrajsingh1491@gmail.com>
2026-09-12 18:54:38 +0530

    Day 1 progress

---
+++ src/components/home/HomeHeroText.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Video from './Video'
+
+const HomeHeroText = () => {
+  return (
+    <div className='h-screen w-screen font-[font1] text-center'>
+        <div className='text-[9.5vw] uppercase leading-[9vw] flex items-center justify-center'>The spark for</div>
+        <div className='w-full text-[9.5vw] uppercase leading-[9vw] flex items-center justify-center'>
+            <div>all</div>
+            <div className='h-[7vw] w-[16vw] rounded-full overflow-hidden -mt-5'>
+                <Video />
+            </div>
+            <div>things</div>
+        </div>
+        <div className='text-[9.5vw] uppercase leading-[9vw] flex items-center justify-center'>creative</div>
+    </div>
+  )
+}
+
+export default HomeHeroText
