716f5671fc3b0be0009c74bc44abbb08f247d5c4
Tilak <tilakrajsingh1491@gmail.com>
2026-09-13 22:33:31 +0530

    Updated Day 1

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -3,10 +3,15 @@ import {Link} from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div className='font-[font2] flex items-center justify-center item-end gap-10'>
-        <Link className='text-[7vw] uppercase border-3 border-white rounded-full leading-[7vw]  px-6 item-bottom'>work</Link>
-        <Link className='text-[7vw] uppercase border-3 border-white rounded-full leading-[7vw]  px-6 item-bottom'>agency</Link>
-    </div>
+    <div className="font-[font2] flex items-center justify-center gap-10">
+  <Link className="text-[7vw] uppercase border-3 border-white rounded-full leading-none mb-0 px-7">
+    work
+  </Link>
+
+  <Link className="text-[7vw] uppercase border-3 border-white rounded-full leading-none mb-0 px-7">
+    agency
+  </Link>
+</div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import Video from '../components/home/Video'
-import HomeHeroText from '../components/home/HomeHeroText'
-import HomeBottomText from '../components/home/HomeBottomText'
+import React from "react";
+import Video from "../components/home/Video";
+import HomeHeroText from "../components/home/HomeHeroText";
+import HomeBottomText from "../components/home/HomeBottomText";
 
 const Home = () => {
   return (
     <div>
-        <div className='h-screen w-screen fixed'>
-            <Video />
+      <div className="h-screen w-screen fixed">
+        <Video />
+      </div>
+      <div className="h-screen w-screen relative flex flex-col">
+        <HomeHeroText />
+        <div className="text-white w-60 self-end font-[font1] text-[14px] text-justify mr-[2vw]">
+          <p>K72 is an agency that builds brands from every angle. Today, tomorrow and years from now. We think the best sparks fly when comfort zones get left behind and friction infuses our strategies, brands and communications with real feeling. We’re transparent, honest and say what we mean, and when we believe in something, we’re all in.</p>
         </div>
-        <div className='h-screen w-screen relative flex flex-col justify-between'>
-            <HomeHeroText />
-            <HomeBottomText />
-        </div>
+        <HomeBottomText />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
